394a0028d49f3f88327e7bbd029273408fcca7f9
Camilo LR <autumndrums@hotmail.com>
2025-04-09 15:43:41 -0300

    Classes de validação de consultas

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,6 +1,11 @@
 package med.voll.api.domain.consulta;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    boolean existsByMedicoAndData(Long idMedico, LocalDateTime data);
 }
